72662d6363b19b822dcc0256656c0f42d67ccff1
Km.Van <kmvan.com@gmail.com>
2015-07-23 11:52:30 +0800

    change link

--- bookmark.php
+++ bookmark.php
@@ -26,7 +26,7 @@
 	<h2>Just click the link from your bookmark when you upload images.</h2>
 	<h3><a href="http://ww3.sinaimg.cn/large/686ee05djw1eihtkzlg6mj216y16ydll.jpg" target="_blank" title="Donate by Alipay">支持开发者 - Donate me by Alipay (QR code)</a></h3>
 	<p>By <a href="<?php echo DES_URL;?>" target="_blank" >INN STUDIO</a></p>
-	<p>由<a href="http://www.bitee.org" target="_blank">“Bitee 比特网舟”</a>提供空间赞助</p>
+	<p class="thanks">由<a href="http://blog.sxbk.pw/" target="_blank">“PS 折腾笔记”</a>提供空间赞助</p>
 	
 </body>
 </html>
